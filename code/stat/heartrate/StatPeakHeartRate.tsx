6331319964d35569838ca78df48a5ba7c4392048
Jacky Lee <aboutjax1990@gmail.com>
2020-03-11 11:54:13 -0400

    update for blog

--- code/stat/heartrate/StatPeakHeartRate.tsx
+++ code/stat/heartrate/StatPeakHeartRate.tsx
@@ -1,48 +1,48 @@
-import * as React from "react";
-import { Frame, addPropertyControls, ControlType } from "framer";
-import { useGlobal } from "reactn";
+import * as React from "react"
+import { Frame, addPropertyControls, ControlType } from "framer"
+import { useGlobal } from "reactn"
 import {
-  metricStyle,
-  labelStyle,
-  statCardStyle,
-  metricAccentStyle,
-  metricStyleLight,
-  labelStyleLight
-} from "../../Styles";
+    metricStyle,
+    labelStyle,
+    statCardStyle,
+    metricAccentStyle,
+    metricStyleLight,
+    labelStyleLight,
+} from "../../Styles"
 // Open Preview (CMD + P)
 // API Reference: https://www.framer.com/api
 
 export function StatPeakHeartRate(props) {
-  const [currentHeartRateArray, setCurrentHeartRateArray] = useGlobal(
-    "heartRateArray"
-  );
+    const [currentHeartRateArray, setCurrentHeartRateArray] = useGlobal(
+        "heartRateArray"
+    )
 
-  let highestData;
+    let peakHeartRate
 
-  if (currentHeartRateArray.length == 0) {
-    highestData = 0;
-  } else {
-    highestData = Math.max(...currentHeartRateArray);
-  }
+    if (currentHeartRateArray.length == 0) {
+        peakHeartRate = 0
+    } else {
+        peakHeartRate = Math.max(...currentHeartRateArray)
+    }
 
-  return (
-    <Frame style={statCardStyle} size={"100%"}>
-      <h1 style={props.theme == "dark" ? metricStyle : metricStyleLight}>
-        {highestData}
-      </h1>
-      <span style={props.theme == "dark" ? labelStyle : labelStyleLight}>
-        bpm (peak)
-      </span>
-    </Frame>
-  );
+    return (
+        <Frame style={statCardStyle} size={"100%"}>
+            <h1 style={props.theme == "dark" ? metricStyle : metricStyleLight}>
+                {peakHeartRate}
+            </h1>
+            <span style={props.theme == "dark" ? labelStyle : labelStyleLight}>
+                bpm (peak)
+            </span>
+        </Frame>
+    )
 }
 
 addPropertyControls(StatPeakHeartRate, {
-  theme: {
-    type: ControlType.SegmentedEnum,
-    defaultValue: "dark",
-    options: ["dark", "light"],
-    optionTitles: ["Dark", "Light"],
-    title: "Theme"
-  }
-});
+    theme: {
+        type: ControlType.SegmentedEnum,
+        defaultValue: "dark",
+        options: ["dark", "light"],
+        optionTitles: ["Dark", "Light"],
+        title: "Theme",
+    },
+})
